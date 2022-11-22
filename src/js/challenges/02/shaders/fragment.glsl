ace0ccb71ce315d746a555ff01fd58b0733e5e83
Andrey Nikonov <a.einomi@gmail.com>
2022-11-22 18:01:47 +0100

    Add two squares

--- src/js/challenges/02/shaders/fragment.glsl
+++ src/js/challenges/02/shaders/fragment.glsl
@@ -4,12 +4,17 @@ varying vec2 vUv;
 varying vec3 vPosition;
 uniform float uTime;
 
-void main(void ) {
-  vec2 size = vec2(1.0);
-  vec2 center = vec2(3.0);
+// returns 1.0 when point pt is inside a rectangle
+// defined by size and center
+float inRect(vec2 size, vec2 center) {
   vec2 squarePos = vPosition.xy - center;
   float horz = step(-size.x, squarePos.x) - step(size.x, squarePos.x);
   float vert = step(-size.y, squarePos.y) - step(size.y, squarePos.y);
-  float result = horz * vert;
-  gl_FragColor = vec4(vec3(1.0, 1.0, 0.0) * result, 1.0);
+  return horz * vert;
+}
+
+void main(void ) {
+  vec3 square1 = vec3(1.0, 1.0, 0.0) * inRect(vec2(0.7), vec2(-2.0, 0.0));
+  vec3 square2 = vec3(0.0, 0.0, 1.0) * inRect(vec2(1.0), vec2(3.0, -1.0));
+  gl_FragColor = vec4(square1 + square2, 1.0);
 }
