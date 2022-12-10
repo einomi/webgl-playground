1773f9e56fef5cdfff86e30d071d27fed31cc592
Andrey Nikonov <a.einomi@gmail.com>
2022-12-10 18:48:20 +0100

    Set up dimensions between webgl and html

--- src/js/challenges/08/images.js
+++ src/js/challenges/08/images.js
@@ -1,4 +1,4 @@
-import { Renderer, Camera, Transform, Box, WireMesh, Vec3 } from 'ogl';
+import { Camera, Mesh, Plane, Program, Renderer, Transform } from 'ogl';
 
 const containerElement = document.querySelector('[data-container]');
 
@@ -7,13 +7,19 @@ const gl = renderer.gl;
 containerElement?.appendChild(gl.canvas);
 gl.canvas.classList.add('canvas-image-grid');
 
-const camera = new Camera(gl);
-camera.position.z = 4;
+gl.clearColor(0, 0, 0, 1);
+
+const cameraDistance = 600;
+
+const camera = new Camera(gl, { fov: 35, near: 0.01, far: 1000 });
+camera.position.set(0, 0, cameraDistance);
 
 const resize = () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
   camera.perspective({
     aspect: gl.canvas.width / gl.canvas.height,
+    fov:
+      2 * Math.atan(window.innerHeight / 2 / cameraDistance) * (180 / Math.PI),
   });
 };
 window.addEventListener('resize', resize, false);
@@ -21,37 +27,38 @@ resize();
 
 const scene = new Transform();
 
-const geometry = new Box(gl);
+const geometry = new Plane(gl, {
+  width: 100,
+  height: 100,
+});
 
-// const program = new Program(gl, {
-//   vertex: /* glsl */ `
-//             attribute vec3 position;
-//
-//             uniform mat4 modelViewMatrix;
-//             uniform mat4 projectionMatrix;
-//
-//             void main() {
-//                 gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-//             }
-//         `,
-//   fragment: /* glsl */ `
-//             void main() {
-//                 gl_FragColor = vec4(0.2);
-//             }
-//         `,
-// });
+const program = new Program(gl, {
+  vertex: /* glsl */ `
+            attribute vec3 position;
 
-const mesh = new WireMesh(gl, {
+            uniform mat4 modelViewMatrix;
+            uniform mat4 projectionMatrix;
+
+            void main() {
+                gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+            }
+        `,
+  fragment: /* glsl */ `
+            void main() {
+                gl_FragColor = vec4(1.0);
+            }
+        `,
+});
+
+const mesh = new Mesh(gl, {
   geometry,
-  wireColor: new Vec3(1, 0.75, 0),
+  program,
 });
 mesh.setParent(scene);
 
 const update = () => {
   requestAnimationFrame(update);
 
-  mesh.rotation.y -= 0.01;
-  mesh.rotation.x += 0.007;
   renderer.render({ scene, camera });
 };
 requestAnimationFrame(update);
