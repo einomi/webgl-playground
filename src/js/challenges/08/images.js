53e7bae810a7108be5f6563f23dda6941f51c197
Andrey Nikonov <a.einomi@gmail.com>
2022-12-10 12:00:57 +0100

    Refactor code in images playground

--- src/js/challenges/08/images.js
+++ src/js/challenges/08/images.js
@@ -1,28 +1,30 @@
 import { Renderer, Camera, Transform, Box, Program, Mesh } from 'ogl';
 
-{
-  const renderer = new Renderer();
-  const gl = renderer.gl;
-  document.body.appendChild(gl.canvas);
+const containerElement = document.querySelector('[data-container]');
 
-  const camera = new Camera(gl);
-  camera.position.z = 5;
+const renderer = new Renderer();
+const gl = renderer.gl;
+containerElement?.appendChild(gl.canvas);
+gl.canvas.classList.add('webgl');
 
-  const resize = () => {
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.perspective({
-      aspect: gl.canvas.width / gl.canvas.height,
-    });
-  };
-  window.addEventListener('resize', resize, false);
-  resize();
+const camera = new Camera(gl);
+camera.position.z = 5;
 
-  const scene = new Transform();
+const resize = () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.perspective({
+    aspect: gl.canvas.width / gl.canvas.height,
+  });
+};
+window.addEventListener('resize', resize, false);
+resize();
 
-  const geometry = new Box(gl);
+const scene = new Transform();
 
-  const program = new Program(gl, {
-    vertex: /* glsl */ `
+const geometry = new Box(gl);
+
+const program = new Program(gl, {
+  vertex: /* glsl */ `
             attribute vec3 position;
 
             uniform mat4 modelViewMatrix;
@@ -32,22 +34,21 @@ import { Renderer, Camera, Transform, Box, Program, Mesh } from 'ogl';
                 gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
             }
         `,
-    fragment: /* glsl */ `
+  fragment: /* glsl */ `
             void main() {
                 gl_FragColor = vec4(1.0);
             }
         `,
-  });
+});
 
-  const mesh = new Mesh(gl, { geometry, program });
-  mesh.setParent(scene);
+const mesh = new Mesh(gl, { geometry, program });
+mesh.setParent(scene);
 
-  const update = () => {
-    requestAnimationFrame(update);
-
-    mesh.rotation.y -= 0.04;
-    mesh.rotation.x += 0.03;
-    renderer.render({ scene, camera });
-  };
+const update = () => {
   requestAnimationFrame(update);
-}
+
+  mesh.rotation.y -= 0.04;
+  mesh.rotation.x += 0.03;
+  renderer.render({ scene, camera });
+};
+requestAnimationFrame(update);
