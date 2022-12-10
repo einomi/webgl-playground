1b1624c087737e99f5b9eeeda5816fd1c951728b
Andrey Nikonov <a.einomi@gmail.com>
2022-12-10 12:46:44 +0100

    Add image grid

---
+++ src/_data/IMAGES.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line unicorn/filename-case
+module.exports = [
+  {
+    src: '/images/image-grid/1.jpg',
+  },
+  {
+    src: '/images/image-grid/2.jpg',
+  },
+  {
+    src: '/images/image-grid/3.jpg',
+  },
+  {
+    src: '/images/image-grid/4.jpg',
+  },
+  {
+    src: '/images/image-grid/5.jpg',
+  },
+  {
+    src: '/images/image-grid/6.jpg',
+  },
+];
